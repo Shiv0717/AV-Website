9ee43dae4e9d1fbe15e3366856bb0b2395c5a0b2
Shiv03072002 <ushiv.kumar@7lives.agency>
2026-01-09 17:16:17 +0530

    update next config for Railway compatibility

--- components/LaunchBanner.jsx
+++ components/LaunchBanner.jsx
@@ -5,7 +5,6 @@ export default function LaunchBanner() {
     <div className="w-full bg-[#0e172a] text-white text-sm">
       <div className="mx-auto max-w-7xl px-4 py-2 text-center">
         🎁 Celebrate our Launch: Enjoy full access of  <strong> app.ankvidya.in</strong> — completely free for early users.{" "}
-        
       </div>
     </div>
   );
